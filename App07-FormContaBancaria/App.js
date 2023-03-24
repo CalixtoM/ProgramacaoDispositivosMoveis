9cb93dba74bdba351432d1243baaba843b0518d1
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2023-03-24 11:51:04 -0300

    exibicao nome e idade

--- App07-FormContaBancaria/App.js
+++ App07-FormContaBancaria/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TextInput, Switch } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Switch, Button } from 'react-native';
 import Constants from 'expo-constants';
 
 // You can import from local files
@@ -17,9 +17,22 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
+      nome: '',
+      idade: '',
+      sexo: '',
+      escolaridade: '',
+      limite: '',
+      brasileiro: '',
       status: false
     };
-  };
+
+    this.cadastrar = this.cadastrar.bind(this);
+  }
+    cadastrar(){
+      this.setState({resultado: 'Nome: ' + this.state.nome + '\n Idade: ' + this.state.idade + '\n Sexo: ' + this.state.sexo})
+
+    }
+    
 
 
   render(){
@@ -30,16 +43,16 @@ class App extends Component{
         </View>
         <View style={styles.campos}>
           <Text style={styles.idCampos}>Nome:</Text>
-          <TextInput style={styles.input} placeholder="informe seu Nome"/>
+          <TextInput style={styles.input} onChangeText={ (valor) => this.setState({nome: valor})} placeholder="informe seu Nome"/>
           <Text style={styles.idCampos}>Idade:</Text>
-          <TextInput style={styles.input} placeholder="informe sua Idade"/>
+          <TextInput style={styles.input} onChangeText={ (valor) => this.setState({idade: valor})} placeholder="informe sua Idade"/>
           <Text style={styles.idCampos}>Sexo:</Text>
-          <Picker>
+          <Picker onChangeText={ (valor) => this.setState({sexo: valor})}>
             <Picker.Item key={1} value={1} label="Masculino" />
             <Picker.Item key={2} value={2} label="Feminino" />
           </Picker>
           <Text style={styles.idCampos}>Escolaridade:</Text>
-          <Picker>
+          <Picker onChangeText={ (valor) => this.setState({escolaridade: valor})}>
             <Picker.Item key={1} value={1} label="Fundamental Incompleto" />
             <Picker.Item key={2} value={2} label="Fundamental Completo" />
             <Picker.Item key={1} value={1} label="Medio Incompleto" />
@@ -48,15 +61,18 @@ class App extends Component{
             <Picker.Item key={2} value={2} label="Superior COmpleto" />
           </Picker>
           <Text style={styles.idCampos}>Limite:</Text>
-          <Slider style={styles.input} minimumValue={0} maximumValue={1200}/>
+          <Slider style={styles.input} onChangeText={ (valor) => this.setState({limite: valor})} minimumValue={0} maximumValue={1200}/>
           <Text style={styles.idCampos}>Brasileiro?</Text>
-          <Switch value={this.state.status} onValueChange={ (valorSwitch) => this.setState({status: valorSwitch})}/>
-
-          <Text style={{textAlign: 'center', fontSize:30}}>
-            {(this.state.status) ? "Ativo" : "Inativo"}
+          <Switch  value={this.state.status} onValueChange={ (valorSwitch) => this.setState({status: valorSwitch})}/>
+          <Text style={{textAlign: 'left', fontSize:16}}>
+            {(this.state.status) ? "Sim" : "Não"}
           </Text>
-
+          <View style={styles.botao}>
+            <Button color = '#1b92f8' title="Cadastrar" onPress={this.cadastrar} />
+            </View>
         </View>
+
+         <Text style={styles.contador}>{ this.state.resultado }</Text>
       </View>
     )
   }
@@ -68,6 +84,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: Constants.statusBarHeight,
+
   },
 
   tituloApp: {
@@ -93,5 +110,8 @@ const styles = StyleSheet.create({
   idCampos: {
     fontWeight: 'bold',
     fontSize: 16
+  },
+  botao: {
+    padding: 60
   }
 });
